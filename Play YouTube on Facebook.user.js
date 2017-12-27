d92965e86c1da5dd6021ea3a2a07295613f3630c
Tommy Miland <tmiland@live.no>
2017-12-27 08:46:50 +0100

    Play YouTube on Facebook
    Play YouTube Videos on Facebook without leaving the site.

---
+++ Play YouTube on Facebook.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name        Play YouTube on Facebook
+// @description Play YouTube Videos on Facebook without leaving the site.
+// @namespace   https://gist.github.com/tmiland/e2a86addea3ef5900b4f5445e17e80ce
+// @version     1.0
+// @date        27-12-2017
+// @author      tmiland
+// @match       https://www.facebook.com/*
+// @require     http://code.jquery.com/jquery-3.2.1.min.js
+// @grant       none
+/* Thanks to https://chrome.google.com/webstore/detail/my-today-song-super-duper/mbnofkhnoflaknikohfaedmdaiafohpg for the code. :)
+   I am not the author of this code, i have just modified it sligtly to my likings, so i can use it as a userscript. */
+// ==/UserScript==
+	(function() {
+	'use strict';
+	$(document).ready(function () {
+		$(document).on("click", 'a', function (event) {
+			var href = $(this).attr('href');
+			href = getId(href);
+			if (href == 'error') {
+				return;
+			}
+
+			event.preventDefault();
+
+			// pause all currently playing YouTube frames
+			$('.youtube_frame').each(function(){
+				this.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+			}
+		);
+			// Old code for reference
+			//$(this).replaceWith('<iframe class="youtube_frame"  src="//www.youtube.com/embed/' + href + '?autoplay=1&enablejsapi=1" frameborder="0" allowfullscreen></iframe>');
+			//Added privacy option
+			$(this).replaceWith('<iframe class="youtube_frame" src="https://www.youtube-nocookie.com/embed/' + href + '?autoplay=1&enablejsapi=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
+		});
+		function getId(url) {
+			var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+			var match = url.match(regExp);
+
+			if (match && match[2].length == 11) {
+				return match[2];
+			} else {
+				return 'error';
+			}
+		}
+	});
+})();
