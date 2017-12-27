4382fa85c08b54fe3485c0c8bf42fc63590f6954
Tommy Miland <tmiland@live.no>
2017-12-27 09:00:57 +0100

    Added width and height

--- Play YouTube on Facebook.user.js
+++ Play YouTube on Facebook.user.js
@@ -2,14 +2,17 @@
 // @name        Play YouTube on Facebook
 // @description Play YouTube Videos on Facebook without leaving the site.
 // @namespace   https://gist.github.com/tmiland/e2a86addea3ef5900b4f5445e17e80ce
-// @version     1.0
+// @version     1.1
 // @date        27-12-2017
 // @author      tmiland
 // @match       https://www.facebook.com/*
 // @require     http://code.jquery.com/jquery-3.2.1.min.js
 // @grant       none
 /* Thanks to https://chrome.google.com/webstore/detail/my-today-song-super-duper/mbnofkhnoflaknikohfaedmdaiafohpg for the code. :)
-   I am not the author of this code, i have just modified it sligtly to my likings, so i can use it as a userscript. */
+   I am not the author of this code, i have just modified it slightly to my likings, so i can use it as a userscript. */
+//---------------------------
+// 1.0 Added privacy option
+// 1.1 Added width and height
 // ==/UserScript==
 	(function() {
 	'use strict';
@@ -30,8 +33,7 @@
 		);
 			// Old code for reference
 			//$(this).replaceWith('<iframe class="youtube_frame"  src="//www.youtube.com/embed/' + href + '?autoplay=1&enablejsapi=1" frameborder="0" allowfullscreen></iframe>');
-			//Added privacy option
-			$(this).replaceWith('<iframe class="youtube_frame" src="https://www.youtube-nocookie.com/embed/' + href + '?autoplay=1&enablejsapi=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
+			$(this).replaceWith('<iframe type="text/html" width="474" height="360" class="youtube_frame" src="https://www.youtube-nocookie.com/embed/' + href + '?autoplay=1&enablejsapi=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
 		});
 		function getId(url) {
 			var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
