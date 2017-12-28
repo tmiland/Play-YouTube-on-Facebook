487ba8359272064f049ce480b26ecaddda3db492
Tommy Miland <tmiland@live.no>
2017-12-28 05:36:22 +0100

    Added update and download URL

--- Play-YouTube-on-Facebook.user.js
+++ Play-YouTube-on-Facebook.user.js
@@ -3,8 +3,10 @@
 // @description Play YouTube Videos on Facebook without leaving the site.
 // @namespace   https://github.com/tmiland/Play-YouTube-on-Facebook
 // @updateURL   https://github.com/tmiland/Play-YouTube-on-Facebook/raw/master/Play-YouTube-on-Facebook.user.js
-// @version     1.3
-// @date        27-12-2017
+// @downloadURL https://github.com/tmiland/Play-YouTube-on-Facebook/raw/master/Play-YouTube-on-Facebook.user.js
+// @supportURL  https://github.com/tmiland/Play-YouTube-on-Facebook/issues
+// @version     1.4
+// @date        28-12-2017
 // @author      tmiland
 // @match       https://www.facebook.com/*
 // @require     http://code.jquery.com/jquery-3.2.1.min.js
@@ -16,6 +18,7 @@
 // 1.1 Added width and height
 // 1.2 Changed @namespace address
 // 1.3 Updated Regex to match more YouTube URLs - 28.12.2017
+// 1.4 Added update and download URL
 // ==/UserScript==
 
 	$(document).ready(function () {
