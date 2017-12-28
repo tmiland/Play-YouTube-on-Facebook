a38b9a8199d0a790cf969df77e2557f09ea26e27
Tommy Miland <tmiland@live.no>
2017-12-28 18:05:29 +0100

    Updated regExp to match url with "-ISM" in the ID.

--- Play-YouTube-on-Facebook.user.js
+++ Play-YouTube-on-Facebook.user.js
@@ -1,3 +1,18 @@
+// ==UserScript==
+// @name        Play YouTube on Facebook
+// @description Play YouTube Videos on Facebook without leaving the site.
+// @namespace   https://github.com/tmiland/Play-YouTube-on-Facebook
+// @updateURL   https://github.com/tmiland/Play-YouTube-on-Facebook/raw/master/Play-YouTube-on-Facebook.user.js
+// @downloadURL https://github.com/tmiland/Play-YouTube-on-Facebook/raw/master/Play-YouTube-on-Facebook.user.js
+// @supportURL  https://github.com/tmiland/Play-YouTube-on-Facebook/issues
+// @version     1.6
+// @date        28-12-2017
+// @author      tmiland
+// @match       https://www.facebook.com/*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @grant       none
+// @license MIT
+// ==/UserScript==
 /**
  ---------------------------------Change-log--------------------------------------
 | 1.0 Added privacy option                                                        |
@@ -6,23 +21,9 @@
 | 1.3 Updated Regex to match more YouTube URLs - 28.12.2017                       |
 | 1.4 Added update and download URL                                               |
 | 1.5 Adjusted width, simplified the regExp and added support for new share links.|
+| 1.6 Updated regExp to match url with "-ISM" in the ID.                          |
  ---------------------------------------------------------------------------------
 **/
-// ==UserScript==
-// @name        Play YouTube on Facebook
-// @description Play YouTube Videos on Facebook without leaving the site.
-// @namespace   https://github.com/tmiland/Play-YouTube-on-Facebook
-// @updateURL   https://github.com/tmiland/Play-YouTube-on-Facebook/raw/master/Play-YouTube-on-Facebook.user.js
-// @downloadURL https://github.com/tmiland/Play-YouTube-on-Facebook/raw/master/Play-YouTube-on-Facebook.user.js
-// @supportURL  https://github.com/tmiland/Play-YouTube-on-Facebook/issues
-// @version     1.5
-// @date        28-12-2017
-// @author      tmiland
-// @match       https://www.facebook.com/*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @grant       none
-// @license MIT
-// ==/UserScript==
 	$(document).ready(function () {
 		$(document).on("click", 'a', function (event) {
 			var href = $(this).attr('href');
@@ -44,15 +45,15 @@
 		});
 		function getId(url) {
 			if(url){
-			//var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-			var regExp = /(youtu.be\/|v[=]|v\/|v%3D)([a-zA-Z0-9]+)&?/;
-			var match = url.match(regExp);
-			if (match && match.length > 2 && match[2].length == 11) {
-				return match[2];
+				//var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+				var regExp = /^.*(youtu.be\/|v[=]|v\/|v%3D-)([-a-zA-Z0-9]+)&?/;
+				var match = url.match(regExp);
+				if (match && match.length > 2 && match[2].length == 11) {
+					return match[2];
 				} else {
 					return 'error';
 				}
 			}
-		    return 'error';
+				return 'error';
 		}
 	});
