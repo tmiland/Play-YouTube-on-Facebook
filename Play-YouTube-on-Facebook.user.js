46216935f1c28670b11cfe7a6cffbc68141f4ae8
Tommy Miland <tmiland@live.no>
2018-12-05 18:46:28 +0100

    Update Play-YouTube-on-Facebook.user.js
    Found a better method

--- Play-YouTube-on-Facebook.user.js
+++ Play-YouTube-on-Facebook.user.js
@@ -5,12 +5,12 @@
 // @updateURL   https://github.com/tmiland/Play-YouTube-on-Facebook/raw/master/Play-YouTube-on-Facebook.user.js
 // @downloadURL https://github.com/tmiland/Play-YouTube-on-Facebook/raw/master/Play-YouTube-on-Facebook.user.js
 // @supportURL  https://github.com/tmiland/Play-YouTube-on-Facebook/issues
-// @version     2.0
-// @date        29-01-2018
+// @version     2.1
+// @date        05-12-2018
 // @author      tmiland
 // @match       https://www.facebook.com/*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
-// @grant       none
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @grant       GM_addStyle
 // @license MIT
 // ==/UserScript==
 /*----------------------------------Change-log-------------------------------------*\
@@ -25,8 +25,11 @@
 | 1.8 Added a regExp to cover ALL URLs, adjusted width.                             |
 | 1.9 Beautified the code. Removed references.                                      |
 | 2.0 Changed URL due to browser error.                                             |
+| 2.1 Found a better method from "YoutTube Player for Facebook"                     |
+|     Extension for Google Chrome @ https://goo.gl/y6puXq /                         |
+|     https://www.facebook.com/beesworkers                                          |
 \*---------------------------------------------------------------------------------*/
-$(document).ready(function () {
+/* old method 
   $(document).on("click", 'a', function (event) {
     var href = $(this).attr('href');
     href = getId(href);
@@ -42,7 +45,99 @@ $(document).ready(function () {
     });
     $(this).replaceWith('<iframe type="text/html" width="476" height="360" class="youtube_frame" src="https://www.youtube.com/embed/' + href + '?autoplay=1&enablejsapi=1" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>');
   });
+*/
 
+let events = ['load'];
+
+events.forEach((event) => {
+  window.addEventListener(event, (e) => {
+    addVideoTags();
+  }, false);
+});
+
+function addVideoTags() {
+  let youtubeLinks = [];
+  const AllLinks = document.querySelectorAll('a[target="_blank"],a[href^="https://www.youtube.com"]');
+
+  for (let i = 0; i < AllLinks.length; i++) {
+    if (
+      (
+        ((AllLinks[i].href.indexOf('https%3A%2F%2Fwww.youtube.com%2FAttribution') > -1
+          || AllLinks[i].href.indexOf('https%3A%2F%2Fwww.youtube.com%2Fattribution') > -1
+          || AllLinks[i].href.indexOf('https%3A%2F%2Fwww.youtube.com%2Fwatch') > -1
+          || AllLinks[i].href.indexOf('watch%253Fv%253D') > -1)
+          || (AllLinks[i].href.indexOf('youtu.be') > -1 && AllLinks[i].href.length >= 28))
+        || (AllLinks[i].href.indexOf('m.youtube.com') > -1 && AllLinks[i].href.indexOf('channel') == -1))
+      && AllLinks[i].className !== ''
+      && (!AllLinks[i].attributes['data-appname'] || AllLinks[i].attributes['data-appname'].value !== "YouTube")
+      && AllLinks[i].href.indexOf('channel') == -1) {
+      youtubeLinks.push(AllLinks[i]);
+    }
+  }
+
+  for (let i = 0; i < youtubeLinks.length; i++) {
+    let unclickable = findAncestor(youtubeLinks[i], 'unclickable');
+    if (unclickable && options.showSuggestedTextShare) {
+      let textarea = findAncestor(youtubeLinks[i], '_5h_u').querySelector('textarea');
+      triggerEvent(textarea, 'keydown');
+      triggerEvent(textarea, 'click');
+      triggerEvent(textarea, 'keypress');
+      textarea.selectionEnd = 0;
+
+      let hiddenInput = findAncestor(youtubeLinks[i], '_5h_u').querySelector('.mentionsHidden');
+    }
+
+    let container = findAncestor(youtubeLinks[i], 'mtm') || findAncestor(youtubeLinks[i], '_3e7u') || findAncestor(youtubeLinks[i], '_10la') || findAncestor(youtubeLinks[i], '_3x-2') || findAncestor(youtubeLinks[i], '_52mr') || findAncestor(youtubeLinks[i], '_2r3x');
+
+    let vidId = getVideoId(youtubeLinks[i]);
+
+    if (container) {
+
+      let elem = document.createElement('iframe');
+      elem.className = "youtube_frame";
+      elem.src = "https://www.youtube.com/embed/" + vidId;
+      elem.width = container.offsetWidth;
+      elem.height = container.offsetWidth * (5 / 9);
+      elem.frameBorder = "0";
+      elem.allowFullscreen = "true";
+
+      if (container.offsetWidth >= 300) {
+        container.parentElement.appendChild(elem);
+        container.remove();
+      }
+    }
+  }
+
+  setTimeout(addVideoTags, 1000);
+}
+
+function findAncestor(el, cls) {
+  while ((el = el.parentElement) && !el.classList.contains(cls));
+  return el;
+}
+
+function getVideoId(link) {
+  if (link.href.indexOf("youtu.be") > -1 || link.href.indexOf("m.youtube.com") > -1) {
+    if (link.href.indexOf('l.facebook.com') > -1) {
+      if (link.href.indexOf("m.youtube.com") > -1)
+        return link.href.substr(link.href.indexOf('v%3D') + 4, 11)
+      else
+        return link.href.substr(link.href.indexOf('be%2F') + 5, 11)
+    } else {
+      return link.href.substr(link.href.length - 11, link.href.length);
+    }
+  } else {
+    let start;
+    if (link.href.indexOf('v%3D') > -1) {
+      start = link.href.indexOf('v%3D') + 4;
+    } else {
+      start = link.href.indexOf('v%253') + 6;
+    }
+    return link.href.substr(start, 11);
+  }
+}
+
+/* Old method
   function getId(url) {
     if (url) {
       var regExp = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|v%3D|%2F|\/)([\w\-_]+)\&?/;
@@ -56,4 +151,15 @@ $(document).ready(function () {
     }
     return 'error';
   }
-});
+*/
+
+function triggerEvent(el, type) {
+  if ('createEvent' in document) {
+    // modern browsers, IE9+
+    var e = document.createEvent('HTMLEvents');
+    e.initEvent(type, false, true);
+    el.dispatchEvent(e);
+  } else {
+     return 'error';
+  }
+}
